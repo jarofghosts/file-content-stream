2bb7726e6b7edfe5f96f787d91072468a22de969
jesse keane <me@jessekeane.me>
2013-07-12 20:31:22 +0400

    removed unnecessary dependency. chill out, its early

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-var dirstream = require('dir-stream'),
-    fs = require('fs'),
+var fs = require('fs'),
     through = require('through');
 
+module.exports = fileStream;
 
+function fileStream() {
+  var tr = through(write, end);
+
+  function write(buf) {
+    var line = 0;
+  }
+
+  function end() {
+    
+  }
+}
