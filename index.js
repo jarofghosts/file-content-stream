60981a87eb630904889cb12a8455aafaf0552d2c
jesse keane <me@jessekeane.me>
2013-10-28 19:42:48 -0700

    fix up a bit

--- index.js
+++ index.js
@@ -1,56 +1,54 @@
 var fs = require('fs'),
     split = require('split'),
-    through = require('through');
+    through = require('through')
 
-module.exports = fileStream;
+module.exports = fileStream
 
 function fileStream(options) {
-  
-  options = options || {};
+
+  options = options || {}
 
   var tr = through(write, noop),
       file = null,
-      files = [];
+      files = []
 
-  return tr;
+  return tr
 
   function processFile(filename) {
     var line = 1,
-        file = fs.createReadStream(filename).pipe(split());
+        file = fs.createReadStream(filename).pipe(split())
     
-    file.on('data', onData);
-    file.on('end', onEnd);
-    file.on('error', noop);
+    file.on('data', onData)
+    file.on('end', onEnd)
+    file.on('error', noop)
 
     function onData(data) {
 
-      if (!data) return ++line;
+      if (!data) return ++line
 
       var fileObject = {
         filename: filename,
         line: line,
         data: data
-      };
+      }
 
-      tr.queue(fileObject);
+      tr.queue(fileObject)
 
-      line++;
+      line++
 
     }
 
     function onEnd() {
-      if (files.length) processFile(files.shift());
+      if (files.length) processFile(files.shift())
     }
-
  }
 
   function write(buf) {
-    files.push(buf.toString());
-    if (file != null) return;
-    file = true;
-    processFile(files.shift());
+    files.push(buf.toString())
+    if (file != null) return
+    file = true
+    processFile(files.shift())
   }
 
   function noop() {}
 }
-
